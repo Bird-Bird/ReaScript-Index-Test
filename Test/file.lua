6a09436d4d2e8edd2ba733dd414506bd1e871db0
BirdBird <55462109+Bird-Bird@users.noreply.github.com>
2022-05-24 18:10:56 +0300

    update

--- Test/file.lua
+++ Test/file.lua
@@ -1,6 +1,7 @@
 -- @description Index Test
--- @version 0.3
+-- @version 0.4.1
 -- @author BirdBird
-
+-- @provides
+--    [nomain][data]libraries/some_file.json
 
 reaper.ShowConsoleMsg("Hello!\n")
